cca506c10908b02a225a7857c94a24cf0aed314d
Muhammad Usman Khan <53698685+usmann123706@users.noreply.github.com>
2023-11-06 21:25:17 +0500

    Update streamlit_app.py
    adding pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 #created the main python file
+import pandas as pd
 import streamlit
 streamlit.title('Hello Usman')
 streamlit.header('Monday Tasks')
 streamlit.text('Learn snowflake')
+fruits=pd.read.csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(fruits)
