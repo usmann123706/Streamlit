9ccb174a8e6afe8124e7556fdc2293a92b4114ab
Muhammad Usman Khan <53698685+usmann123706@users.noreply.github.com>
2023-11-06 21:26:16 +0500

    Update streamlit_app.py
    fixing errors while working with pandas library

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import streamlit
 streamlit.title('Hello Usman')
 streamlit.header('Monday Tasks')
 streamlit.text('Learn snowflake')
-fruits=pd.read.csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+fruits=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(fruits)
