b9a3b0ca5f7a326e278c8f7a8d903a6f78a741d1
Muhammad Usman Khan <53698685+usmann123706@users.noreply.github.com>
2023-11-06 22:16:43 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ streamlit.header('Monday Tasks')
 streamlit.text('Learn snowflake')
 fruits_read=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruits_index=fruits_read.set_index('Fruit')
-fruits_multi=streamlit.multiselect("Select fruits:",list(fruits_index.index),['Apple','Banana'])
+fruits_multi=streamlit.multiselect("Select fruits:",list(fruits_index.index))
 fruits=fruits_index.loc[fruits_multi]
 streamlit.dataframe(fruits)
