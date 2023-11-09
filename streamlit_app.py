927e69abe85c6cc4a3d25cc701cc6b0e3c4ea779
Muhammad Usman Khan <53698685+usmann123706@users.noreply.github.com>
2023-11-09 21:08:48 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit
 import requests
 import snowflake.connector
 from urllib.error import URLError
+def get_fruityvice_Date(fruit_choice):
+  response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  response_json = response.json()
+  return response_json
 streamlit.title('Hello Usman')
 streamlit.header('Monday Tasks')
 streamlit.text('Learn snowflake')
@@ -17,9 +21,8 @@ try:
   if not fruit_choice:
     streamlit.error("please enter fruit to get information")
   else:
-    response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    response_json = response.json()
-    response_normalized = pd.json_normalize(response_json)
+    back_from_function= get_fruityvice_Date(fruit_choice)
+    response_normalized = pd.json_normalize(back_from_functio)
     streamlit.dataframe(response_normalized)
 except URLError as e:
   streamlit.error()
