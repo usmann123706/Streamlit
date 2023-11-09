86cc935dba5fd98229dcf7aa3783912345e2b35d
Muhammad Usman Khan <53698685+usmann123706@users.noreply.github.com>
2023-11-09 21:01:00 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-#created the main python file
 import pandas as pd
 import streamlit
 import requests
 import snowflake.connector
+from urllib.error import URLError
 streamlit.title('Hello Usman')
 streamlit.header('Monday Tasks')
 streamlit.text('Learn snowflake')
@@ -12,11 +12,17 @@ fruits_multi=streamlit.multiselect("Select fruits:",list(fruits_index.index))
 fruits=fruits_index.loc[fruits_multi]
 streamlit.dataframe(fruits)
 streamlit.header('Json response')
-fruit_choice=streamlit.text_input("Which fruit information would you like to get ?",'orange')
-response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-response_json = response.json()
-response_normalized = pd.json_normalize(response_json)
-streamlit.dataframe(response_normalized)
+try: 
+  fruit_choice=streamlit.text_input("Which fruit information would you like to get ?")
+if not fruit_choice:
+  streamlit.error("please enter fruit to get information")
+else
+  response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  response_json = response.json()
+  response_normalized = pd.json_normalize(response_json)
+  streamlit.dataframe(response_normalized)
+except URLError as e:
+  streamlit.error()
 my_connector=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cursor=my_connector.cursor()
 my_cursor.execute("select * from FRUIT_LOAD_LIST")
